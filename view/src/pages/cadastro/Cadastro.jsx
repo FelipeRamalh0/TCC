ffdab757ee763f19240e5830a79fbe31eb0f3df4
henriquebispo16 <riquebispo4@gmail.com>
2026-04-24 20:54:40 -0300

    melhorei tela de cadastro e adicionei esqueci senha

--- view/src/pages/cadastro/Cadastro.jsx
+++ view/src/pages/cadastro/Cadastro.jsx
@@ -1,31 +1,84 @@
+// Importa o arquivo de estilos (CSS)
 import './Cadastro.css'
 
-function Cadastro(){
+// Criação do componente Cadastro
+function Cadastro() {
 
+    // O return define o que será exibido na tela
     return (
-        <div className='container'>
-            <form>
-                <h1>Cadastrar</h1>
-                <input name='nome' type='text' placeholder='Insira seu nome'/>
-                <input name='email' type='email' placeholder='Coloque seu Email'/>
-                <input name='senha' type='password' placeholder='Digite sua senha'/>
 
-                <input name='idade' type='number' placeholder='Coloque sua idade'
-                min='10' max='100'/>
-              
-            
-                <select name="tipo">
-                    <option value="">Selecione o seu nível</option>                  
-                    <option value="iniciante">Iniciante</option>
-                    <option value="intermediario">Intermediário</option>
-                    <option value="profissional">Profissional</option>
-                </select>
+        // Container principal (fundo da tela)
+        <div className="container">
 
-            <button type='button'>Cadastrar</button>
-            </form>
+            {/* Card central (caixa branca onde fica o formulário) */}
+            <div className="card">
+
+                {/* Formulário de cadastro */}
+                <form>
+
+                    {/* Título principal */}
+                    <h1>Cadastrar</h1>
+
+                    {/* Texto de apoio */}
+                    <p>Preencha os campos abaixo para criar sua conta</p>
+
+                    {/* Campo para digitar o nome */}
+                    <input 
+                        type="text" 
+                        placeholder="Insira seu nome" 
+                    />
+
+                    {/* Campo de email (já valida formato automaticamente) */}
+                    <input 
+                        type="email" 
+                        placeholder="Coloque seu Email" 
+                    />
+
+                    {/* Campo de senha (texto oculto) */}
+                    <input 
+                        type="password" 
+                        placeholder="Digite sua senha" 
+                    />
+
+                    {/* Campo numérico para idade */}
+                    <input 
+                        type="number" 
+                        placeholder="Coloque sua idade" 
+                        min="10"  // valor mínimo permitido
+                        max="100" // valor máximo permitido
+                    />
+
+                    {/* Campo de seleção (dropdown) */}
+                    <select>
+
+                        {/* Opção padrão */}
+                        <option>Selecione o seu nível</option>
+
+                        {/* Opções disponíveis */}
+                        <option>Iniciante</option>
+                        <option>Intermediário</option>
+                        <option>Profissional</option>
+
+                    </select>
+
+                    {/* Botão de envio do formulário */}
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+
+                    {/* Texto com link para login */}
+                    <span>
+                        Já tem uma conta? <a href="#">Faça login</a>
+                    </span>
+
+                    <span><a href="forgot"></a></span>
+
+                
+                </form>
+            </div>
         </div>
-        
     )
 }
 
+// Exporta o componente para poder usar em outros arquivos
 export default Cadastro
