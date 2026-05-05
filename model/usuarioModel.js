b86e5940abac4368d5a136dae09e5c85d9918e55
Aluno <e023aluno@outlook.com>
2026-05-05 19:54:19 -0300

    UsuarioModel terminado

--- model/usuarioModel.js
+++ model/usuarioModel.js
@@ -10,3 +10,43 @@ export async function criarUsuario(dados) {
     return result.insertId;
 }
 
+export async function listarUsuários(){
+    const [rows]= await db.query(
+        `SELECT nome, email, tipo_usuario FROM Usuarios`
+    );
+    return rows;
+}
+
+export async function buscarId(id){
+    const [rows]= await db.query(
+        `SELECT id_usuario, nome, email, tipo_usuario FROM Usuarios
+        WHERE id_usuario= ?`, [id]
+    );
+    return rows[0];
+}
+
+export async function buscarEmail(email) {
+    const [rows]= await db.query(
+        `SELECT * FROM Usuarios WHERE email= ?`, [email]
+    );
+    return rows[0];
+}
+
+export async function atualizarUsuario(id, dados){
+    const {nome, email}= dados;
+
+    const [result]= await db.query(
+        `UPDATE Usuarios set nome = ?, email = ?, WHERE = id_usuario= ? `,
+        [nome], [email], [id]
+    )
+    return result.affectedRows
+}
+
+export async function deletarUsuario(id){
+    const [result]= await db.query(
+        `DELETE FROM Usuarios WHERE id_usuario = ?`
+    )
+    return result.affectedRows;
+}
+
+
