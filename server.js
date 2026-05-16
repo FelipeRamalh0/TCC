e26340dace241d1eb8067d8a3e12b4ee24451a58
FelipeRamalh0 <felipe.cnramalho@gmail.com>
2026-05-16 01:41:58 -0300

    Html feitos mas não esta funcionando todos os botões por causa de rota

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path"
 import { usuariosRoutes } from "./routes/usuarioRoute.js";
 import { tarefaRoutes } from "./routes/tarefaRoute.js";
 const PORT= process.env.PORT || 3000;
@@ -7,7 +8,11 @@ const PORT= process.env.PORT || 3000;
 const app= express();
 app.use(express.json());;
 app.use(cors());
+app.use(express.static("view"));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("view/index.html"));
+});
 
 app.use(usuariosRoutes);
 app.use(tarefaRoutes);
