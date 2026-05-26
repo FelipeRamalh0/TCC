8360997cc3d8d4d506e509f756190c2d2df4bb68
Aluno <e023aluno@outlook.com>
2026-05-25 22:16:29 -0300

    Model da recuperacao senha feito

---
+++ model/recuperacaoModel.js
@@ -0,0 +1,42 @@
+import { db } from "../dbConfig/dbConfig";
+
+export async function salvarToken(dados) {
+    
+    const {
+        id_usuario,
+        token,
+        expiracao
+    } = dados
+    const [result]= await db.query(
+        `INSERT INTO Recuperacao_Senha(
+        id_usuario, token, expiracao) VALUES (?, ?, ?)`,
+        [id_usuario, token, expiracao]
+    );
+    return result.insertId
+}
+
+export async function buscarToken(token){
+const [result]= await db.query(
+    `SELECT * FROM Recuperacao_Senha
+    WHERE token= ? and usado = FALSE
+    and expiracao > NOW()`,
+    [token]
+);
+return rows[0]
+}
+// Marcar token usado
+export async function usarToken(token){
+
+    const [result] = await db.query(
+
+        `UPDATE Recuperacao_Senha
+
+         SET usado = TRUE
+
+         WHERE token = ?`,
+
+        [token]
+    );
+
+    return result.affectedRows;
+}
