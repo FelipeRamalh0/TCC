0b7973cb242faca44023c06326f4020eb4cc954e
Aluno <e023aluno@outlook.com>
2026-05-27 19:36:21 -0300

    Revert "sim"
    This reverts commit 520ef24ae7fdff7c60e94491f3f1bdd47d5fa270.

--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -1,105 +1,12 @@
-import express from "express";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { connection } from "../database/connection.js";
+import { Router } from "express";
+import { cadastrar, login } from "../controller/authController.js";
+import path from "path";
+const router= Router();
 
-export const usuariosRoutes = express.Router();
-
-
-/* =========================
-   LOGIN
-========================= */
-
-usuariosRoutes.post("/login", (req, res) => {
-
-    const { email, senha } = req.body;
-
-    const sql = `
-
-        SELECT *
-        FROM Usuarios
-        WHERE email = ?
-
-    `;
-
-    connection.query(
-
-        sql,
-
-        [email],
-
-        async (err, result) => {
-
-            if (err) {
-
-                return res.status(500).json({
-                    erro: "Erro no servidor"
-                });
-
-            }
-
-            if (result.length === 0) {
-
-                return res.status(404).json({
-                    erro: "Usuário não encontrado"
-                });
-
-            }
-
-            const usuario = result[0];
+router.post('/cadastrar', cadastrar);
+router.post('/login', login);
 
 
-            /* VERIFICA SENHA */
-
-            const senhaCorreta =
-                await bcrypt.compare(
-                    senha,
-                    usuario.senha_hash
-                );
 
 
-            if (!senhaCorreta) {
-
-                return res.status(401).json({
-                    erro: "Senha incorreta"
-                });
-
-            }
-
-
-            /* TOKEN */
-
-            const token = jwt.sign(
-
-                {
-                    id: usuario.id_usuario
-                },
-
-                "segredo"
-
-            );
-
-
-            /* RETORNO */
-
-            res.json({
-
-                token,
-
-                usuario: {
-
-                    id: usuario.id_usuario,
-                    nome: usuario.nome,
-                    email: usuario.email,
-                    tipo_usuario:
-                        usuario.tipo_usuario
-
-                }
-
-            });
-
-        }
-
-    );
-
-});
+export const usuariosRoutes= router;
--- server.js
+++ server.js
@@ -1,62 +1,27 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-
+import path from "path"
 import { usuariosRoutes } from "./routes/usuarioRoute.js";
 import { tarefaRoutes } from "./routes/tarefaRoute.js";
-import { recuperacaoRoutes } from "./routes/recuperacaoRoute.js";
-import { entregaRoutes } from "./routes/entregaRoute.js";
+import { recuperacaoRoutes } from "./routes/recuperacaoRoute.js"
+import { entregaRoutes} from "./routes/entregaRoute.js"
+const PORT= process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-
-/* =========================
-   CONFIGURAÇÕES
-========================= */
-
-app.use(express.json());
-
+const app= express();
+app.use(express.json());;
 app.use(cors());
-
 app.use(express.static("view"));
 
-
-/* =========================
-   PÁGINA INICIAL
-========================= */
-
 app.get("/", (req, res) => {
-
-    res.sendFile(
-        path.resolve("view/index.html")
-    );
-
+    res.sendFile(path.resolve("view/index.html"));
 });
-
-
-/* =========================
-   ROTAS
-========================= */
 
 app.use(usuariosRoutes);
-
 app.use(tarefaRoutes);
-
 app.use(recuperacaoRoutes);
+app.use(entregaRoutes)
 
-app.use(entregaRoutes);
 
-
-/* =========================
-   SERVIDOR
-========================= */
-
-app.listen(PORT, () => {
-
-    console.log(
-        `Servidor rodando em: http://localhost:${PORT}`
-    );
-
-});
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando em: http://localhost:${PORT}`);
+})
